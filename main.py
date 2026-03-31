250b6764b289a1ac8821105861e6b6e10ed824fe
子猫Isis <isis.martini@escola.pr.gov.br>
2026-03-31 11:35:46 -0300

    Refactor main.py for input handling and categorization
    Corrected the indentation and added print statements for user prompts.

--- main.py
+++ main.py
@@ -4,6 +4,25 @@ contagem_categorias= {
     "Bom": 0,
     "Recuperação": 0,
     "Reprovados" : 0,
-    
-    
 }
+print("--- Registro de Notas (25 Alunos) ---")
+for i in range(1,26):
+    while True:
+        try:
+            nota = float(input(f"digite a nota do aluno {i}: "))
+            if 0 <= nota <= 10:
+                notas.append(nota)
+                
+                if nota >= 9:
+                    contagem_categorias["Excelente"]
+                elif nota >=7:
+                    contagem_categorias["Bom"]
+                elif nota >=5:
+                    contagem_categorias["Recuperação"]
+                else:
+                    contagem_categorias["reprovado"]
+                break
+            else:
+                print("Por favor, digite uma nota válida entre 0 e 10.")
+        except ValueError:
+            print("Entrada inválida. Digite um número.")
