f307bba65f671f60df566b7af4217736e8efec2b
子猫Isis <isis.martini@escola.pr.gov.br>
2026-04-14 11:29:54 -0300

    Fix category counting in student grades input

--- main.py
+++ main.py
@@ -12,15 +12,14 @@ for i in range(1,26):
             nota = float(input(f"digite a nota do aluno {i}: "))
             if 0 <= nota <= 10:
                 notas.append(nota)
-                
                 if nota >= 9:
-                    contagem_categorias["Excelente"]
-                elif nota >=7:
-                    contagem_categorias["Bom"]
-                elif nota >=5:
-                    contagem_categorias["Recuperação"]
+                    contagem_categorias["Excelente"] += 1
+                elif nota >= 7:
+                    contagem_categorias["Bom"] += 1
+                elif nota >= 5:
+                    contagem_categorias["Recuperação"] += 1
                 else:
-                    contagem_categorias["reprovado"]
+                    contagem_categorias["Reprovados"] += 1
                 break
             else:
                 print("Por favor, digite uma nota válida entre 0 e 10.")
@@ -34,7 +33,7 @@ menor_nota = min(notas)
 print("\n" + "=" * 30)
 print("RELATORIO FINAL DA TURMA")
 print("=" * 30)
-print(f"MC)dia Geral da Turma: {media_geral:.2f}")
+print(f"Media Geral da Turma: {media_geral:.2f}")
 print(f"Maior Nota: {maior_nota:.2f}")
 print(f"Menor Nota: {menor_nota:.2f}")
 print("-" * 30)
